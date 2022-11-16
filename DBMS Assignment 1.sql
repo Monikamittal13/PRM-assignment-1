d9c895a1832400ef614a5fbf1803d7593c4d7e7b
Monikamittal13 <110057510+Monikamittal13@users.noreply.github.com>
2022-11-16 23:36:40 +0530

    Update DBMS Assignment 1.sql

--- DBMS Assignment 1.sql
+++ DBMS Assignment 1.sql
@@ -1,4 +1,4 @@
 DROP table IF EXISTS countries;
-create table countries(country_id varchar(2), country_name varchar(30) CHECK (country_name IN('Italy','India','China')), region_id int);
+create table countries(country_id varchar(2) UNIQUE PRIMARY KEY, country_name varchar(30) CHECK (country_name IN('Italy','India','China')), region_id int);
 
 desc countries;
